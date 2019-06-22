47b4b25fdd23e3e432ab807d295ebc726a935a53
superunix <david.lili@outlook.com>
2019-06-22 21:54:10 +1000

    git user.name correction

--- Test1.py
+++ Test1.py
@@ -5,6 +5,6 @@ section1 = random.randint(1, 255)
 section2 = random.randint(1, 255)
 section3 = random.randint(1, 255)
 section4 = random.randint(1, 255)
-
+#测试
 random_ip = str(section1) + '.' + str(section2) + '.' + str(section3) + '.' + str(section4)
 print(random_ip)
